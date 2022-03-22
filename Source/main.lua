1396155cf71ec49aea0f7ed1cce429b6ab326c10
Marco Carandente <marco.carandente@deltacygnilabs.com>
2022-03-22 21:26:48 +0200

    Reworked check collision functions
    Modularizing a bit, fixing error in iterative method, added backbone for checking kinds of collisions

--- Source/main.lua
+++ Source/main.lua
@@ -22,10 +22,10 @@ local currentRadius = 50
 local playerPosition = geometry.point.new(180,100)
 local previousPlayerPosition =  playerPosition:copy()
 local circleCenter = geometry.point.new(200,120)
-local playerSpeed = geometry.vector2D.new(0,0)
+local playerSpeed = geometry.vector2D.new(20,10)
 local playerSpinSpeed = 0
 local currentOpeningSize = 20
-local gravity = 200
+local gravity = 400
 local playerRadius = 10
 local playerRotation = 0
 local circleLineWidth = 5
@@ -33,33 +33,35 @@ local playerLineWidth = 3
 local distanceFromCenter = 0
 local collisionRelocationMagnitude = 0
 local contactFriction = .8
-local bounceElasticity = .9
+local bounceElasticity = .8
 local airFriction = .9
 local collidedThisFrame = false
 local terminalVelocity = 400
 local terminalSpinning = 400
+local innerRadius = 10
 
 function playdate.update()
     gfx.clear()
     playdate.drawFPS(0,0)
     playdate.timer.updateTimers()
-    collidedThisFrame = false
+    collidedThisFrame = ""
     collisionRelocationMagnitude = 0
     previousPlayerPosition = playerPosition:copy()
     --checkCommands()
     debugMovePlayerWithbuttons()
     checkCrank()
-    drawCircle()
+    drawCircles()
     updatePlayerPosition()
     updatePlayerSpeed()
     drawPlayer()
     playdate.resetElapsedTime()
 end
 
-function drawCircle()
+function drawCircles()
     gfx.setLineWidth(circleLineWidth)
     gfx.setStrokeLocation(gfx.kStrokeInside)
     gfx.drawArc(circleCenter.x, circleCenter.y, currentRadius, playdate.getCrankPosition()+currentOpeningSize,playdate.getCrankPosition()+(360-currentOpeningSize))
+    gfx.fillCircleAtPoint(circleCenter,innerRadius)
 end
 
 function checkCommands()
@@ -92,24 +94,29 @@ function checkCrank()
 end
 
 function updatePlayerPosition()
-    playerPosition.y +=  playerSpeed.y * playdate.getElapsedTime()
-    playerPosition.x += playerSpeed.x * playdate.getElapsedTime()
-    if checkCollision() then
-        collidedThisFrame = true      
-        -- iterative method
-        local currentSegment = geometry.lineSegment.new(previousPlayerPosition.x,previousPlayerPosition.y,playerPosition.x,playerPosition.y)
-        local midpoint = currentSegment:midPoint()
-        for i = 10, 1, 1
-        do
-            midpoint = currentSegment:midPoint()
-            if (checkCircleCollision(circleCenter, currentRadius, midpoint)) then
-                currentSegment = geometry.lineSegment.new(previousPlayerPosition.x,previousPlayerPosition.y,midpoint.x,midpoint.y)
-            else
-                currentSegment = geometry.lineSegment.new(midpoint.x,midpoint.y,PlayerPosition.x,PlayerPosition.y)
-            end
-        end
-        if(checkCircleCollision(circleCenter, currentRadius, midpoint))then
-            playerPosition = midpoint:copy()
+   playerPosition.y +=  playerSpeed.y * playdate.getElapsedTime()
+   playerPosition.x += playerSpeed.x * playdate.getElapsedTime()
+
+   checkCollision()
+   
+   if collidedThisFrame == "outer" then     
+      local currentSegment = geometry.lineSegment.new(previousPlayerPosition.x,previousPlayerPosition.y,playerPosition.x,playerPosition.y)
+      for i = 1, 10, 1
+      do
+         local currentSegment = geometry.lineSegment.new(previousPlayerPosition.x,previousPlayerPosition.y,playerPosition.x,playerPosition.y)
+           local midpoint = currentSegment:midPoint()
+           for i = 1, 10, 1
+           do
+               midpoint = currentSegment:midPoint()
+               if (checkInnerCircleCollision(circleCenter, currentRadius, midpoint)) then
+                   currentSegment = geometry.lineSegment.new(previousPlayerPosition.x,previousPlayerPosition.y,midpoint.x,midpoint.y)
+               else
+                   currentSegment = geometry.lineSegment.new(midpoint.x,midpoint.y,PlayerPosition.x,PlayerPosition.y)
+               end
+           end
+      end
+
+        if(checkInnerCircleCollision(circleCenter, currentRadius, playerPosition))then
             -- shift towards circle center 
             correctionDirectionVector = playerPosition - circleCenter
             playerPosition.x = playerPosition.x + (correctionDirectionVector.dx / correctionDirectionVector:magnitude() * -(playerRadius - (currentRadius- distanceFromCenter - circleLineWidth)))
@@ -117,34 +124,36 @@ function updatePlayerPosition()
         else
             playerPosition = midpoint:copy()
         end        
-    end
+      --playerPosition = previousPlayerPosition:copy()
+   end
     
-    playerRotation += playerSpinSpeed * playdate.getElapsedTime()
+   playerRotation += playerSpinSpeed * playdate.getElapsedTime()
 
 end
 
 function updatePlayerSpeed()
 
-   if (collidedThisFrame) then
-        -- case collision with line circle, bouncing
-        originalNormalVector = circleCenter - playerPosition
-        normalizedNormal = originalNormalVector:normalized()
-        playerSpeed = playerSpeed - (normalizedNormal:scaledBy((2 * normalizedNormal:dotProduct(playerSpeed))))
-        
+      -- case collision with inside of circle 
+   if(collidedThisFrame == "outer") then
+      originalNormalVector = circleCenter - playerPosition
+      normalizedNormal = originalNormalVector:normalized()
+      -- bouncing
+      playerSpeed = playerSpeed - (normalizedNormal:scaledBy((2 * normalizedNormal:dotProduct(playerSpeed))))
+      
         -- adding speed from spinning speed and reducing 
         playerSpeed.x += playerSpinSpeed * normalizedNormal:rightNormal().dx * contactFriction
         playerSpeed.y += playerSpinSpeed * normalizedNormal:rightNormal().dy * contactFriction
-        playerSpinSpeed -= contactFriction * playdate.getElapsedTime() * playerSpinSpeed
-
+        playerSpinSpeed -= contactFriction * playdate.getElapsedTime() * playerSpinSpeed 
         playerSpeed *= bounceElasticity
-        
-        
         playerSpinSpeed -= playdate.getCrankChange() * contactFriction / currentRadius * playerRadius
         
-   else
+   elseif (collidedThisFrame == "") then
         playerSpinSpeed -= airFriction * playdate.getElapsedTime() * playerSpinSpeed
    end
+   
    playerSpeed.y += playdate.getElapsedTime() * gravity   
+
+   -- to update with math.floor and ceiling if they get implemented in the apis   
    if(playerSpeed.x > terminalVelocity)then
       playerSpeed.x = terminalVelocity
    elseif (playerSpeed.x < -terminalVelocity) then
@@ -165,23 +174,35 @@ function updatePlayerSpeed()
 end
 
 
-function checkCircleCollision (center, radius, point)
-    distanceFromCenter = point:distanceToPoint(center) 
-    if  distanceFromCenter + playerRadius > currentRadius - circleLineWidth then
-        return true
-    else
+function checkOuterCircleCollision (center, radius, point)
+   distanceFromCenter = point:distanceToPoint(center) 
+   if  distanceFromCenter + playerRadius > currentRadius - circleLineWidth then
+
+       return true
+   else
         return false
-    end
+   end
 end
     
+function checkInnerCircleCollision (center, radius, point)
+     distanceFromCenter = point:distanceToPoint(center) 
+     if  distanceFromCenter - playerRadius < radius then
+        return true 
+     else
+         return false
+     end
+end
+ 
 function checkCollision()
-    
-    -- outline circle detection
-    distanceFromCenter = playerPosition:distanceToPoint(circleCenter) 
-    if  distanceFromCenter + playerRadius > currentRadius - circleLineWidth then
-        return true
-    else
-        return false
+   if checkOuterCircleCollision(circleCenter, currentRadius, playerPosition) then
+        collidedThisFrame = "outer"
+        print("outer", playdate.getElapsedTime())
+   elseif checkInnerCircleCollision(circleCenter, innerRadius, playerPosition) then
+        collidedThisFrame = "inner"
+        print("inner", playdate.getElapsedTime())
+
+   else 
+       collidedThisFrame = ""
     end
 end
 
@@ -212,5 +233,3 @@ end
 function playdate.gameWillResume()
     playdate.resetElapsedTime()
 end
-
-
